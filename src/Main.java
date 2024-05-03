dad1c7ca827a15e24edb0ce9811a5bfeaa3a8b68
KinaSw <163517470+KinaSw@users.noreply.github.com>
2024-05-03 15:07:05 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,38 @@
 public class Main {
-    public static void main(String[] args) {
-       
-                Segment segment = new Segment(new Point(0,0),new Point(100,100));
-                System.out.println(segment.toSvg());
-                Segment[] segments = Segment.perpendicularSegments(segment,new Point(100/2,100/2));
-                System.out.println(segment.length());
-                System.out.println(segments[0].toSvg());
-                System.out.println(segments[1].toSvg());
-                System.out.println(segments[0].length());
-                System.out.println(segments[1].length());
 
-                Point[] pointsArray = {new Point(50, 50), new Point(50, 20), new Point(100, 20),new Point(100, 50)};
-                Polygon polygon = new Polygon(pointsArray);
-                System.out.println(polygon.toSvg());
 
-    }
+        public static void main(String[] args){
+            Segment[] arr = new Segment[2];
 
+                float x1;
+                float y1;
+                float max = 5;
+                float min = 1;
+                x1 = (float)(Math.random() * (max - min)+ 1) + min;
+                y1 = (float)(Math.random() * (max - min)+ 1) + min;
+                SvgScene scene = SvgScene.getScene();
+
+                Shape ellipse = new Ellipse(new Style("yellow", "green", 3), new Vec2(300, 500), 100, 200);
+                ellipse = new SolidFillShapeDecorator(ellipse, "purple");
+                ellipse = new DropShadowDecorator(ellipse);
+
+
+
+                Polygon poly = new Polygon(new Vec2[]{new Vec2(200, 10), new Vec2(550, 50), new Vec2(100, 100)});
+
+                TransformationDecorator.Builder builder = new TransformationDecorator.Builder(new SolidFillShapeDecorator(poly, "red"));
+                TransformationDecorator s =
+                        builder
+                                .rotate(new Vec2(0, 0), 30)
+                                .scale(new Vec2(1, 1.5))
+                                .build();
+
+
+                scene.add(s);
+                scene.add(ellipse);
+
+                scene.save("result.html");
+
+
+        }
 }
