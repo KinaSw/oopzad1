d135007f837e7e2e91ef72b3312194bd5e7a7d4a
KinaSw <163517470+KinaSw@users.noreply.github.com>
2024-05-03 15:08:10 +0200

    Update Polygon.java

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,18 +1,37 @@
-public class Polygon {
-    private Point[] points;
-    public Polygon(Point[] points){
-        this.points = points;
-    }
-    public Polygon(Polygon polygon){
 
-    }
-    public String toSvg(){
-        StringBuilder poly = new StringBuilder();
-        poly.append("polygon point = \"");
-        for(Point point: points){
-            poly.append(point.x).append(", ").append(point.y).append(" ");
+public class Polygon implements Shape{
+        private Vec2[] points;
+
+        public Polygon(Vec2[] points) {
+            this.points = points;
         }
-        poly.append("\" style=\"fill:none;stroke:black;stroke-width:1\"/>\n");
-        return poly.toString();
-    }
+        public Polygon(Polygon polygon)
+        {
+            this.points = new Vec2[polygon.points.length];
+            for(int i = 0; i<polygon.points.length; i++)
+            {
+                points[i] = new Vec2(polygon.points[i].x, polygon.points[i].y);
+            }
+        }
+        @Override
+        public String toSvg(String parameters){
+            StringBuilder poly = new StringBuilder();
+            poly.append("\n<polygon points=\"");
+            for(Vec2 point: points){
+                poly.append(point.x).append(",").append(point.y).append(" ");
+            }
+            poly.append("\"");
+            poly.append(parameters);
+            poly.append("/>");
+            return poly.toString();
+        }
+        public static Polygon square(Segment s){
+            double halfLen = s.distance()/2;
+            Vec2 center = new Vec2((s.getP2().x + s.getP1().x)/2, (s.getP2().y + s.getP1().y)/2);
+            Segment half = new Segment(center, s.getP1());
+            Segment[] parts = Segment.perpend(half, center, halfLen);
+            return new Polygon(new Vec2[]{s.getP1(), parts[0].getP2(), s.getP2(), parts[1].getP2()});
+        }
+
+
 }
